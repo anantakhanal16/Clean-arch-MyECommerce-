efad6e100fd76ad759e8383a6432aeddc6c350bf
anantakhanal16 <anantaramankhanal@gmail.com>
2023-12-27 19:07:45 +0545

    Merge changes from remote repository

---
+++ Mye-CommerceApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Mye_CommerceApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
