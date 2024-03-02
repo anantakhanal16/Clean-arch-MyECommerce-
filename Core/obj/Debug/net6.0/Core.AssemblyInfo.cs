f51576c6ac74ddf4885f14708b93e2982d34154a
anantakhanal16 <anantaramankhanal@gmail.com>
2024-03-02 18:11:45 +0545

    Added exception middleware ..

--- Core/obj/Debug/net6.0/Core.AssemblyInfo.cs
+++ Core/obj/Debug/net6.0/Core.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b2278f18ceeb1fb929a35f995c65cdbe4bb56971")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3dd7ed031f32b6365a2add4f8b9e8f94794bb790")]
 [assembly: System.Reflection.AssemblyProductAttribute("Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ Infrastructure/Middlewares/ExceptionHandlingMiddlewares.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+
+namespace Infrastructure.Middlewares
+{
+    public class ExceptionHandlingMiddlewares
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger<ExceptionHandlingMiddlewares> _logger;
+
+        public ExceptionHandlingMiddlewares(RequestDelegate next, ILogger<ExceptionHandlingMiddlewares> logger)
+        {
+            _next = next;
+            _logger = logger;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try 
+            {
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+                LogException(ex);
+                throw ex;
+
+            }
+        }
+        private void LogException(Exception ex)
+        {
+            _logger.LogError(ex, ex.Message);
+        }
+    }
+}
